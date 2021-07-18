b920cbe1bad7bcedc9645ea88b06a6169e26278f
Saan Kim <kssan99@gmail.com>
2021-07-19 04:35:48 +0900

    Initial commit

---
+++ src/routes/hello-[hi]/index.svelte
@@ -0,0 +1,16 @@
+<script context="module">
+  let hi;
+  export async function load(ctx) {
+    hi = ctx.page.params.hi;
+
+    return { props: { hi } };
+  }
+</script>
+
+<main>
+    <deus>
+        <h1>
+            {hi}
+        </h1>
+    </deus>
+</main>
